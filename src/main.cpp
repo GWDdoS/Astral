4863919e6032e843b99130595d78f19e6c9d26cb
slideglide <68702247+slideglide@users.noreply.github.com>
2025-06-03 22:54:30 +0300

    i hope this mod builds

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 
-using namespace geode::prelude;
+bool init() {
+    if (!MenuLayer::init())
+    return false;
+
+    auto alert = FLAlertLayer::create(
+        "Hello",
+        "The mod built, Now we can rethink our life choices!",
+        "YAY"
+    );
+       alert->m_scene = this;
+       alert->show();
+
+       return true;
+    }
+};
