4c813defdea852e1325bf5d3653a3fab17888c8e
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-07-22 21:26:56 -0500

    text thingy

--- src/GUI/components/Text.cpp
+++ src/GUI/components/Text.cpp
@@ -1,8 +1,5 @@
 #include "components.hpp"
 
 void Core::Components::Text(const char* text) {
-    char buf[128];
-    snprintf(buf, sizeof(buf), "%s", text);
-
-    ImGui::Text(buf);
+    ImGui::Text("%s", text);
 }
