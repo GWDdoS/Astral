fe56597115cc1fbc6fb9672c810175461f94f39f
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-07-30 10:57:41 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ $execute {
         "open-core"_spr,
         "Toggle CORE",
         "Toggle the CORE UI.",
-        { Keybind::create(cocos2d::enumKeyCodes::KEY_ALT, Modifier::None) },
+        { Keybind::create(cocos2d::enumKeyCodes::KEY_Alt, Modifier::None) },
         "CORE Bot"
     });
     new EventListener([=](InvokeBindEvent* event) {
