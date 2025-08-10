423d26f74b40887b6c6d682fcf43e47e0e11ec3d
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-08-10 16:24:41 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <geode.custom-keybinds/include/Keybinds.hpp>
 #include "includes.h"
+
+// de3am is a boykisser
 
 /* de3am is a boykisser and a list helper
    Breuhh is also a boykisser and a list helper. (de3am told me to put this) 
@@ -9,51 +12,35 @@ using namespace geode::prelude;
 
 bool rendering = false;
 
-void setup() {
-    Astral::GUI::Theme::Astral
-    ();
-}
-// The REAL GUI THING IS HERE
-void render() {
-	Astral::Components::Begin(80, 120, 520, 430, "Astral");
 
-    if (Astral::Components::Button("Test")) {
-        Astral::Renderer::InstallFFmpeg();
-    }
-
-    if (Astral::Components::Button("Render")) {
-        if(!rendering) {
-            rendering = true;
-        } else {
-            rendering = false;
-        }
-    }
-    
-    if (Astral::Components::Button("Save Render (Doesn't work YET)")) {
-        Astral::Renderer::SaveRender();
-    }
-
-    Astral::Components::End();
-}
-
-$on_mod(Loaded) {
-    ImGuiCocos::get().setup(setup).draw(render);
-
-    if (!std::filesystem::exists(AstralPath()))
-        std::filesystem::create_directories(AstralPath());
-    
-    if (!std::filesystem::exists(FFmpegDir()))
-        std::filesystem::create_directories(FFmpegDir());
-
+@@ -47,27 +47,31 @@ $on_mod(Loaded) {
     if (!std::filesystem::exists(RendersDir()))
         std::filesystem::create_directories(RendersDir());
 }
+
+// de3am is a boykisser
+
 class $modify(MenuLayer) {
     void onMoreGames(CCObject* target) {
         Astral_GUI_Mobile_UI::create()->show();
     }
 };
 
+$execute {
+    using namespace keybinds;
+
+    BindManager::get()->registerBindable({
+        "open-astral"_spr,
+        "Toggle Astral",
+        "Toggle the Astral UI.",
+        { Keybind::create(cocos2d::enumKeyCodes::KEY_P, Modifier::Alt) },
+        "CORE Bot"
+    });
+    new EventListener([=](InvokeBindEvent* event) {
+        if (event->isDown()) ImGuiCocos::get().toggle();
+        return ListenerResult::Propagate;
+    }, InvokeBindFilter(nullptr, "open-imgui"_spr));
+}
 void draw() {
     static bool m_show = true;
 
@@ -76,3 +63,5 @@ void draw() {
         ImGui::End();
     }
 } 
+
+
