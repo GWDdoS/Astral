9d42938ee2da086b433391010924a12819fac6d1
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-08-12 10:58:49 -0400

    removed imgui flags

--- src/GUI/components/Tab.cpp
+++ src/GUI/components/Tab.cpp
@@ -6,7 +6,7 @@ void Astral::Components::Begin(int xpos, int ypos, int width, int height, const 
     ImGui::SetNextWindowPos(ImVec2(xpos, ypos), ImGuiCond_Always);
     ImGui::SetNextWindowSize(ImVec2(width, height), ImGuiCond_Always);
 
-    ImGuiWindowFlags flags = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove;
+    /* ImGuiWindowFlags flags = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove; */
     ImGui::Begin(text, open, flags);
 }
 
