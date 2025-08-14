e94be4e71f13e70226e36207193ec74b97dbdb0f
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-08-13 22:52:51 -0400

    Update Tab.cpp

--- src/GUI/components/Tab.cpp
+++ src/GUI/components/Tab.cpp
@@ -5,9 +5,9 @@
 void Astral::Components::Begin(int xpos, int ypos, int width, int height, const char* text, bool* open) {
     ImGui::SetNextWindowPos(ImVec2(xpos, ypos));
     ImGui::SetNextWindowSize(ImVec2(width, height));
-
+    /*
     ImGuiWindowFlags flags = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove;
-    ImGui::Begin(text, open, flags);
+    ImGui::Begin(text, open, flags);*/
 }
 
 void Astral::Components::End() {
