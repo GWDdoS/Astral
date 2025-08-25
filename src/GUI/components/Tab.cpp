c73e23d7be0b250a88451c525286fd037e0d9f66
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-08-24 22:37:48 -0400

    ohh and these

--- src/GUI/components/Tab.cpp
+++ src/GUI/components/Tab.cpp
@@ -5,9 +5,10 @@
 void Astral::Components::Begin(int xpos, int ypos, int width, int height, const char* text, bool* open) {
     ImGui::SetNextWindowPos(ImVec2(xpos, ypos));
     ImGui::SetNextWindowSize(ImVec2(width, height));
-    
+
     ImGuiWindowFlags flags = ImGuiWindowFlags_NoScrollbar;
     ImGui::Begin(text, open, flags);
+    // https://github.com/GMDPLUTONIC/gd-imgui-cocos
 }
 
 void Astral::Components::End() {
