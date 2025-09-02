ef10b6557bda7c8f9a84bdf98559fc1c7d53f606
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-02 15:09:30 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,22 @@ $on_mod(Loaded) {
 	});
 }
 
+$on_mod(Loaded) {
+    ImGuiCocos::get().setup([] {
+        // this runs after imgui has been setup,
+        // its a callback as imgui will be re initialized when toggling fullscreen,
+        // so use this to setup any themes and or fonts!
+    }).draw([] {
+        ImGui::Begin("My awesome window");
+
+        ImGui::Button("Awesome button");
+        
+        ImGui::End();
+    });
+}
+
+
+
 #ifndef GEODE_IS_IOS
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 class $modify(ImGuiKeybindHook, cocos2d::CCKeyboardDispatcher) {
