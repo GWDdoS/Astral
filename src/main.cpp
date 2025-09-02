bf14bb99969bb585e2086c9c39886033e523c3c7
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-02 15:12:53 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ $on_mod(Loaded) {
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 class $modify(ImGuiKeybindHook, cocos2d::CCKeyboardDispatcher) {
 	bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
-		if (key == cocos2d::enumKeyCodes::KEY_RightAlt && isKeyDown) {
+		if (key == cocos2d::enumKeyCodes::KEY_Alt && isKeyDown) {
 			ImGuiCocos::get().toggle();
 		}
 		return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
