15ed877d8def117442f72ccab970043d5fdf229a
slideglide <68702247+slideglide@users.noreply.github.com>
2025-09-03 00:47:56 +0300

    hi

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,10 +1,10 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include "includes.h"
+#include "src/includes.h"
 
 using namespace geode::prelude;
 
-class $modify(MyPlaylayer, PlayLayer) {
+class $modify(MyPlayLayer, PlayLayer) {
     void destroyPlayer(PlayerObject* p1, GameObject* p2) {
         if (!&noclipEnabled) {
             if (!m_anticheatSpike) {
