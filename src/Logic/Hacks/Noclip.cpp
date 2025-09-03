a16837652d1aaedeedbce6fd533f3e31422ca543
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-03 11:29:43 -0400

    Update Noclip.cpp

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,23 +1,6 @@
-
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include "../../includes.h"
-/*
-using namespace geode::prelude;
-
-class $modify(MyPlayLayer, PlayLayer) {
-    void destroyPlayer(PlayerObject* p1, GameObject* p2) {
-        if (noclipEnabled) {
-            // When noclip is enabled, don't destroy the player
-            return;
-        }
-        // When noclip is disabled, call the original function
-        PlayLayer::destroyPlayer(p1, p2);
-    }
-};
-*/
-// omfg tf isgoing on HAAAHHHHAHAHAHAHAHHHAHAHHA
-
 
 class $modify (NoclipPL, PlayLayer) {
     void destroyPlayer(PlayerObject * p0, GameObject * p1) {
