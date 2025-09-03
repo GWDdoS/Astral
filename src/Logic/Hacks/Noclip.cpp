18f9acb858dde53b520e87497e4b5996a5378d4e
slideglide <68702247+slideglide@users.noreply.github.com>
2025-09-03 17:39:28 +0300

    Update Noclip.cpp

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -22,8 +22,10 @@ class $modify(MyPlayLayer, PlayLayer) {
 class $modify (NoclipPL, PlayLayer) {
     void destroyPlayer(PlayerObject * p0, GameObject * p1) {
         if (noclipEnabled) {
+          if (!m_anticheatSpike) {
             return;
         }
+      }
             PlayLayer::destroyPlayer(p0, p1);
     }
 };
