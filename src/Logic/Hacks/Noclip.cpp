d9e2ba1cd737191854f09d62e282579669100d4b
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-03 21:34:16 -0400

    Update Noclip.cpp

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,14 +1,14 @@
-
 #include "../../includes.hpp"
 #include <Geode/modify/PlayLayer.hpp>
 
-class $modify (NoclipPL, PlayLayer) {
-    void destroyPlayer(PlayerObject * p0, GameObject * p1) {
+class $modify(NoclipPL, PlayLayer) {
+    void destroyPlayer(PlayerObject* p0, GameObject* p1) {
         if (noclipEnabled) {
-          if (!m_anticheatSpike) {
-            return;
+            if (!m_anticheatSpike) {
+               
+                return;
+            }
         }
-      }
-            PlayLayer::destroyPlayer(p0, p1);
+        PlayLayer::destroyPlayer(p0, p1);
     }
 };
