e0715ab0fe5ce0f0188e0a952e5e9dea32fa51db
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-04 12:28:16 -0400

    Update Noclip.cpp

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,15 +1,12 @@
 #include "../../includes.hpp"
 
-// Add variables for individual player noclip control
 
 
 class $modify(NoclipPL, PlayLayer) {
     void destroyPlayer(PlayerObject* player, GameObject* obstacle) {
-        // Check which player is being destroyed
         bool isPlayer1 = (player == m_player1);
         bool isPlayer2 = (player == m_player2);
         
-        // Determine if noclip should apply to this specific player
         bool shouldNoclip = false;
         if (isPlayer1 && (noclipEnabled || noclipP1Enabled)) {
             shouldNoclip = true;
@@ -18,15 +15,11 @@ class $modify(NoclipPL, PlayLayer) {
         }
         
         if (shouldNoclip) {
-            // When noclip is enabled for this player, only allow death from anticheat spikes
             if (m_anticheatSpike) {
                 PlayLayer::destroyPlayer(player, obstacle);
             }
-            // Don't call PlayLayer::destroyPlayer for regular obstacles
-            // This prevents the player from dying to normal spikes, blocks, etc.
             return;
         } else {
-            // When noclip is disabled for this player, allow normal death behavior
             PlayLayer::destroyPlayer(player, obstacle);
         }
     }
