9458435ef081ded80b4a1483388516a7ae643224
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-05 13:28:41 -0400

    fix

--- src/vars.h
+++ src/vars.h
@@ -41,7 +41,7 @@ extern int fontList;
 //Chars
 extern char macroName[128];
 extern const char *backgroundThemeNames[];
-extern char fontType[];
+extern char fontType[128];
 
 // Other
 extern cocos2d::enumKeyCodes capturedCustomKey;
