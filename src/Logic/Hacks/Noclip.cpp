ef509b36fc209f515f61c4e017cbffb67a198d59
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-04 20:43:30 -0400

    forgor about this one 💀

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,37 +1,46 @@
 #include "../../includes.hpp"
 
-
 // noclipEnabled = false;
 // noclipP1 = false;
 // noclipP2 = false;
 
-class $modify(NoclipPL, PlayLayer) {
-    void destroyPlayer(PlayerObject* player, GameObject* obstacle) {
+class $modify(NoclipPL, PlayLayer)
+{
+    void destroyPlayer(PlayerObject *player, GameObject *obstacle)
+    {
         bool isPlayer1 = (player == m_player1);
         bool isPlayer2 = (player == m_player2);
         bool shouldNoclip = false;
-        
-        
-        if (noclipEnabled) {
+
+        if (noclipEnabled)
+        {
             shouldNoclip = true;
-        } else {
-            if (isPlayer1 && noclipP1 && noclipEnabled) {
-                shouldNoclip = true;
-            } else if (isPlayer2 && noclipP2 && noclipEnabled) {
-                shouldNoclip = true;
-            } 
         }
-        
-        if (shouldNoclip) {
-            if (m_anticheatSpike) {
+        else
+        {
+            if (isPlayer1 && noclipP1 && noclipEnabled)
+            {
+                shouldNoclip = true;
+            }
+            else if (isPlayer2 && noclipP2 && noclipEnabled)
+            {
+                shouldNoclip = true;
+            }
+        }
+
+        if (shouldNoclip)
+        {
+            if (m_anticheatSpike)
+            {
                 PlayLayer::destroyPlayer(player, obstacle);
-            } else {
+            }
+            else
+            {
                 return;
             }
         }
     }
 };
-
 
 /*
 if (ImGui::Combo("Noclip Mode", &noclipMode, "Off\0Both Players\0Player 1 Only\0Player 2 Only\0")){
@@ -42,19 +51,19 @@ if (ImGui::Combo("Noclip Mode", &noclipMode, "Off\0Both Players\0Player 1 Only\0
                                 noclipP1 = false;
                                 noclipP2 = false;
                                 break;
-            
+
                             case 1: // Both Players
                                 noclipEnabled = true;
                                 noclipP1 = true;
                                 noclipP2 = true;
                                 break;
-            
+
                             case 2: // Player 1 Only
-                                noclipEnabled = true;  
+                                noclipEnabled = true;
                                 noclipP1 = true;
                                 noclipP2 = false;
                                 break;
-            
+
                             case 3: // Player 2 Only
                                 noclipEnabled = true;
                                 noclipP1 = false;
