c4fb32166a8a279fa1b72b33dde4713e9f7ab4dc
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-08 21:08:59 -0400

    Update gui.cpp

--- src/GUI/gui.cpp
+++ src/GUI/gui.cpp
@@ -22,17 +22,19 @@ void setupImGuiStyle()
     auto& style = ImGui::GetStyle();
     auto& io = ImGui::GetIO();
     auto* font = ImGui::GetIO().Fonts->AddFontFromFileTTF((Mod::get()->getResourcesDir() / ("font" + std::to_string(fontType) + ".ttf")).string().c_str(), 16.0f);
-    // style.Alpha = 0.99f; // bg opacity  
+    style.Alpha = 0.99f; // bg opacity  
     style.WindowRounding = 12.0f; // rounding between menus (it also says rounding dumbass)
     style.ChildRounding = 8.0f; 
     style.FrameRounding = 8.0f;
-    //style.PopupRounding = 8.0f;
-    //style.ScrollbarRounding = 12.0f;
-    //style.GrabRounding = 8.0f;
-    //style.TabRounding = 6.0f;
-    //style.WindowPadding = ImVec2(15, 15); // Spaces between shit
-    //style.FramePadding = ImVec2(10, 6);
-    //style.ItemSpacing = ImVec2(12, 8);
+    style.PopupRounding = 8.0f;
+    style.ScrollbarRounding = 12.0f;
+    style.GrabRounding = 8.0f;
+    style.TabRounding = 6.0f;
+    style.WindowPadding = ImVec2(15, 15); // Spaces between shit
+    style.FramePadding = ImVec2(10, 6);
+    style.ItemSpacing = ImVec2(12, 8);
+    style.AntiAliasedLines = true; // i was told this fixes lines and shit, but i bet it wont do anything.
+    style.AntiAliasedFill = true;
     
     styleApplied = true;
 }
