708d39900a0a42d6b970a636c34c5769b7f1e0ad
slideglide <68702247+slideglide@users.noreply.github.com>
2025-09-09 14:46:33 +0300

    safe mode

---
+++ src/Logic/Hacks/SafeMode.cpp
@@ -0,0 +1,37 @@
+#include "../../includes.hpp"
+
+using namespace geode::prelude;
+
+class $modify(SafeLayer, PlayLayer) {
+    void showNewBest(bool newReward, int orbs, int diamonds, bool demonKey, bool noRetry, bool noTitle) {
+        if (safeMode) {
+            return;
+        }
+        else {
+            PlayLayer::showNewBest(bool newReward, int orbs, int diamonds, bool demonKey, bool noRetry, bool noTitle)
+        }
+    }
+    
+    void levelComplete() {
+        if (safeMode) {
+            bool testMode = m_isTestMode;
+            m_isTestMode = true;
+            PlayLayer::levelComplete();
+            m_isTestMode = testMode;
+        }
+        else {
+            PlayLayer::levelComplete();
+        }
+    }
+};
+
+class $modify(GJGameLevel) {
+    void savePercentage(int p0, bool p1, int p2, int p3, bool p4) {
+        if (safeMode) {
+            return;
+        }
+        else {
+            GJGameLevel::savePercentage(p0, p1, p2, p3, p4);
+        }
+    }
+};
