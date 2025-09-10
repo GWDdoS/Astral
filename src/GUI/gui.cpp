bdd25a0fcd03547d238c035733e757752f127bfa
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-10 00:14:48 -0400

    Update gui.cpp

--- src/GUI/gui.cpp
+++ src/GUI/gui.cpp
@@ -132,7 +132,7 @@ void renderMainGui()
     guiVisible = imguiCocos.isVisible();
     if (tabCount <= 0) return;
 
-    ImGuiWindowFlags flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoRsize;
+    ImGuiWindowFlags flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize;
     if (!ImGui::Begin("Astral [BETA]", nullptr, flags)) {
         ImGui::End();
         return;
