e9d67eb67b62b0ce5738cfae9a046d8f25540898
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-10 18:07:32 -0400

    MORE FUCKING SYNTAX

--- src/GUI/gui.cpp
+++ src/GUI/gui.cpp
@@ -58,13 +58,13 @@ void renderHacksTab()
     if (ImGui::BeginMenu("")){
         ImGui::Checkbox("Player 1", &noclipP1);
         ImGui::Checkbox("Player 2", &noclipP2);
-        ImGui::Checkbox("Both", &noclipP2, #&noclipP1);
+        ImGui::Checkbox("Both", &noclipP2, &noclipP1);
         ImGui::EndMenu();
     }    
     ImGui::Spacing();
     ImGui::Checkbox("Speedhack", &speedhackEnabled);
     ImGui::SameLine();
-    ImGui::InputFloat(""&speedhackMultiplier);
+    ImGui::InputFloat("", &speedhackMultiplier);
     if (speedhackMultiplier < 0.f) {
         speedhackMultiplier =1.f;
     }
