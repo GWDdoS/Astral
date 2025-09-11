97801ac82a2b7541722bfbf6a64232e54b4cfb77
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-11 17:32:06 -0400

    i forgot half the code no shit it broke

--- src/Logic/Hacks/TPSBypassEclipse.cpp
+++ src/Logic/Hacks/TPSBypassEclipse.cpp
@@ -4,21 +4,74 @@
 
 #include "../../includes.hpp"
 
-
-class $modify(TPSBypassHook, GJBaseGameLayer) {
+class $modify(TPSBypassGJBGLHook, GJBaseGameLayer) {
     struct Fields {
-        double extraDelta = 0.0;
+        double m_extraDelta = 0.0;
     };
-    
+
+    float getCustomDelta(float dt, float tps) {
+        auto spt = 1.f / tps;
+        
+        auto totalDelta = dt + m_extraDelta;
+        auto timestep = std::min(m_gameState.m_timeWarp, 1.f) * spt;
+        auto steps = std::round(totalDelta / timestep);
+        auto newDelta = steps * timestep;
+        m_extraDelta = totalDelta - newDelta;
+        return static_cast<float>(newDelta);
+    }
+
+    float getModifiedDelta(float dt) {
+        return getCustomDelta(dt, tpsValue);
+    }
+
     void update(float dt) override {
         auto fields = m_fields.self();
-        fields->extraDelta += dt;
-        
-        auto spt = 1.0 / tpsValue; // ticks PER second
-        auto steps = std::round(fields->extraDelta / spt);
-        auto newDelta = steps * spt;
-        fields->extraDelta -= newDelta;
-        
-        GJBaseGameLayer::update(newDelta);
+        fields->m_extraDelta += dt;
+
+        auto timeWarp = std::min(m_gameState.m_timeWarp, 1.f);
+        auto newTPS = tpsValue / timeWarp;
+        auto spt = 1.0 / newTPS;
+        auto steps = std::round(fields->m_extraDelta / spt);
+        auto totalDelta = steps * spt;
+        fields->m_extraDelta -= totalDelta;
+
+        GJBaseGameLayer::update(totalDelta);
+    }
+};
+
+// we should rewrite these at the least
+
+class $modify(TPSBypassPLHook, PlayLayer) {
+    // fix percentage calculation also got this from eclipse who would have known
+    int calculationFix() {
+        auto timestamp = m_level->m_timestamp;
+        auto currentProgress = m_gameState.m_currentProgress;
+        if (timestamp > 0 && tpsValue != 240.f) {
+            auto progress = (m_gameState.m_levelTime * tpsValue) / timestamp * 100.f;
+            m_gameState.m_currentProgress = timestamp * progress / 100.f;
+        }
+        return currentProgress;
+    }
+    // fixes progress bar, also got this from eclipse
+    void updateProgressbar() {
+        auto currentProgress = calculationFix();
+        PlayLayer::updateProgressbar();
+        m_gameState.m_currentProgress = currentProgress;
+    }
+
+    void destroyPlayer(PlayerObject* player, GameObject* object) override {
+        auto currentProgress = calculationFix();
+        PlayLayer::destroyPlayer(player, object);
+        m_gameState.m_currentProgress = currentProgress;
+    }
+    // fixes level time or smt. I got this from eclipse
+    void levelComplete() {
+        auto oldTimestamp = m_gameState.m_unkUint2;
+        if (tpsValue != 240.f) {
+            auto ticks = static_cast<uint32_t>(std::round(m_gameState.m_levelTime * 240));
+            m_gameState.m_unkUint2 = ticks;
+        }
+        PlayLayer::levelComplete();
+        m_gameState.m_unkUint2 = oldTimestamp;
     }
 };
