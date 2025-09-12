133fd6973f7d3c4fac3c937d5496e33617820e63
slideglide <68702247+slideglide@users.noreply.github.com>
2025-09-12 14:48:43 +0300

    rewrite noclip

--- src/Logic/Hacks/Noclip.cpp
+++ src/Logic/Hacks/Noclip.cpp
@@ -1,43 +1,25 @@
 #include "includes.hpp"
 
-// noclipEnabled = false;
-// noclipP1 = false;
-// noclipP2 = false;
-// noclipBoth = false;
+using namespace geode::prelude;
 
-class $modify(NoclipPL, PlayLayer)
-{
-    void destroyPlayer(PlayerObject* player, GameObject* obstacle)
-    {
-        if (noclipEnabled)
-        {
-        bool shouldNoclip = false;
-
-        {
-            if (player == m_player1)
-            {
+class $modify(NoclipPlayLayer, PlayLayer) {
+    void destroyPlayer(PlayerObject* player, GameObject* object) {
+        if (noclipEnabled) {
+            if (player == m_player1) {
                 shouldNoclip = noclipP1;
             }
-            else if (player == m_player2)
-            {
+            else if (player == m_player2) {
                 shouldNoclip = noclipP2;
             }
-        }
 
-        if (shouldNoclip)
-        {
-            if (obstacle == m_anticheatSpike)
-            {
-                PlayLayer::destroyPlayer(player, obstacle);
-            }
-            else
-            {
-                return;
+            if (shouldNoclip) {
+                if (object == m_anticheatSpike) {
+                    PlayLayer::destroyPlayer(player, obstacle);
+                }
+                else {
+                    return;
+                }
             }
         }
-    if (!shouldNoclip) {
-        PlayLayer::destroyPlayer(player, obstacle);
     }
-    }
-  }
 };
