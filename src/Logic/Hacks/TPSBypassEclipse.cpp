bbe9ec6470ae269fed57179796d83ec77a995b09
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-15 02:13:54 -0400

    Update TPSBypassEclipse.cpp

--- src/Logic/Hacks/TPSBypassEclipse.cpp
+++ src/Logic/Hacks/TPSBypassEclipse.cpp
@@ -1,40 +1,40 @@
 #include "../../includes.hpp"
 
 namespace Astral::Hacks::Global {
-
-
+    
+    // i dont really know how this works...? but uhh it kinda does. Need to fix it being really slow and need to fix it not going under 240
     class $modify(GJBaseGameLayer) {
         struct Fields {
             double m_extraDelta = 0.0;
         };
-
+        
         void update(float dt) override {
             if (!tpsEnabled || tpsValue == 240.f || !PlayLayer::get()) {
                 return GJBaseGameLayer::update(dt);
             }
-
+            
             if (framestepEnabled) {
                 return GJBaseGameLayer::update(1.f / tpsValue);
             }
-
+            
             float newDt = 1.f / tpsValue;
             int steps = std::clamp(static_cast<int>(dt / newDt), 1, 4);
-
+            
             for (int i = 0; i < steps; ++i) {
                 GJBaseGameLayer::update(newDt);
             }
         }
-
+        
         float getModifiedDelta(float dt) {
             if (!tpsEnabled || tpsValue == 240.f || !PlayLayer::get()) {
                 return GJBaseGameLayer::getModifiedDelta(dt);
             }
-
+            
             if (m_resumeTimer > 0) {
                 m_resumeTimer--;
                 return 0.0f;
             }
-
+            
             float timeWarp = std::min(m_gameState.m_timeWarp, 1.f);
             float newDt = 1.f / tpsValue;
             
@@ -46,7 +46,7 @@ namespace Astral::Hacks::Global {
             return finalDt;
         }
     };
-
+    // ty eciplse, im going to sleep
     class $modify(PlayLayer) {
         void updateProgressbar() {
             auto timestamp = m_level->m_timestamp;
@@ -60,7 +60,7 @@ namespace Astral::Hacks::Global {
             PlayLayer::updateProgressbar();
             m_gameState.m_currentProgress = currentProgress;
         }
-
+        
         void destroyPlayer(PlayerObject* player, GameObject* object) override {
             auto timestamp = m_level->m_timestamp;
             auto currentProgress = m_gameState.m_currentProgress;
@@ -73,7 +73,7 @@ namespace Astral::Hacks::Global {
             PlayLayer::destroyPlayer(player, object);
             m_gameState.m_currentProgress = currentProgress;
         }
-
+        
         void levelComplete() {
             auto oldTimestamp = m_gameState.m_unkUint2;
             if (tpsValue != 240.f && tpsEnabled) {
