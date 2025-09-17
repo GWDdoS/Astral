773cb06c34b2729e4b792791b7f4b1c581301de4
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-17 01:31:39 -0400

    Update TPSBypass.cpp

--- src/Logic/Hacks/TPSBypass.cpp
+++ src/Logic/Hacks/TPSBypass.cpp
@@ -1,6 +1,6 @@
 #include "../../includes.hpp"
-#include <../../builds/assembler.hpp>
-#include <sinaps.hpp> // idk its in eclipse, lets just see for now
+#include "../../builds/assembler.hpp"
+#include "sinaps.hpp" // idk its in eclipse, lets just see for now
 Also need to implement fps to keep speed the same
 namespace Astral::Hacks::Global {
     
