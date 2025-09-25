6e935a1db91290b8b748063527203a23a5c04452
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-24 21:34:13 -0400

    if this doesnt fix ill remvoe for now

--- src/Logic/Hacks/AccuratePercentage.cpp
+++ src/Logic/Hacks/AccuratePercentage.cpp
@@ -2,19 +2,36 @@
 // someone asked for me to showcase wiht this one time so here it is
 bool accuratePercentage = false;
 int decimalPlaces = 4;
-
+// i was told this would work? bro idk ill just remove this tbh
 class $modify(PlayLayer) {
+    float customGetProgress() {
+        try {
+            return utils::getActualProgress(this);
+        } catch (...) {
+            if (m_player1 && m_levelLength > 0) {
+                float percent = (m_player1->getPositionX() / m_levelLength) * 100.0f;
+                if (percent > 100.0f) percent = 100.0f;
+                if (percent < 0.0f) percent = 0.0f;
+                return percent;
+            }
+            return this->getCurrentPercent();
+        }
+    }
+    
     void updateProgressbar() {
         PlayLayer::updateProgressbar();
         
         if (!accuratePercentage || !m_percentageLabel) return;
         
-        float percent = utils::getActualProgress(this);
+        float percent = customGetProgress();
         m_percentageLabel->setString(fmt::format("{:.{}f}%", percent, decimalPlaces).c_str());
-        m_progressFill->setTextureRect({
-            0, 0,
-            (m_progressBar->getTextureRect().getMaxX() - 5) * percent / 100.f,
-            m_progressBar->getTextureRect().getMaxY() / 2
-        });
+        
+        if (m_progressFill && m_progressBar) {
+            m_progressFill->setTextureRect({
+                0, 0,
+                (m_progressBar->getTextureRect().getMaxX() - 5) * percent / 100.f,
+                m_progressBar->getTextureRect().getMaxY() / 2
+            });
+        }
     }
 };
