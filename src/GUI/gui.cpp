57f331a157cdf89436dc07b8803a7f6e9d9b8418
GWDdoS <132668764+GWDdoS@users.noreply.github.com>
2025-09-27 03:08:57 -0400

    i move

--- src/GUI/gui.cpp
+++ src/GUI/gui.cpp
@@ -8,37 +8,8 @@ const int tabCount = 6;
 
 int currentTab = 0;
 float themeColor[3] = {0.0f, 0.0f, 0.0f};
-float frameCount = 0.0f;
-bool initialized = false;
-static float lastLevelTime = 0.0f;
-static float lastProgress = 0.0f;
-float getCurrentFrame() {
-    auto* playLayer = PlayLayer::get();
-    if (!playLayer) {
-        frameCount = 0.0f;
-        initialized = false;
-        lastLevelTime = 0.0f;
-        lastProgress = 0.0f;
-        return 0.0f;
-    }
-    
-    float currentTime = playLayer->m_gameState.m_levelTime;
-    float currentProgress = playLayer->m_gameState.m_currentProgress;
-    
-    if (initialized && (currentProgress < lastProgress - 0.01f || currentTime < lastLevelTime - 0.01f)) {
-        frameCount = 0.0f;
-        initialized = false;
-        lastLevelTime = 0.0f;
-    }
-    if (!playLayer->m_hasCompletedLevel && 
-        !playLayer->m_isPaused && 
-        currentProgress > 0.0f) {
-            initialized = true;
-        }
-        lastProgress = currentProgress;
-        
-        return frameCount;
-    }
+
+
     #ifdef GEODE_IS_DESKTOP
     
     void applyBackgroundTheme()
